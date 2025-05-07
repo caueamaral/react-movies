fc8a09f72ecc8f580affa964569e49c90dbeabe3
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-07 11:22:11 -0300

    Feat: Google Fonts with GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import NotFound from './pages/NotFound'
+import { createGlobalStyle } from 'styled-components'
 import './css/reset.css'
-import './css/app.css'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Rubik', sans-serif;
+  }
+
+  main {
+      padding: 30px;
+      margin-inline: auto;
+      max-width: 1500px;
+    }
+`
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <GlobalStyle />
         <Header />
           <Routes>
             <Route path="/" element={<Home />} />
