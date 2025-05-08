c7fdedfcb755bf43e15baa28a38fbc0f6d934d7f
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-08 15:33:17 -0300

    Refactor: From .then() to async/await

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -10,24 +10,21 @@ const StyledArticle = styled.article`
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 `
 
-// const movies = [
-//     { id: 1, title: 'Hunger Games 1' },
-//     { id: 2, title: 'Hunger Games 2' },
-//     { id: 3, title: 'Hunger Games 3' },
-//     { id: 4, title: 'Hunger Games 4' },
-//     { id: 5, title: 'Hunger Games 5' },
-//     { id: 6, title: 'Hunger Games 6' }
-// ]
-
 export default function Movies() {
     const [movies, setMovies] = useState<MovieProps[]>([])
 
     useEffect(() => {
-        MoviesApi()
-            .then(response => {
+        const fetchMovies = async () => {
+            try {
+                const response = await MoviesApi()
                 setMovies(response.data.results)
-            })
-    }, [])    
+            } catch(error) {
+                console.error('Faile to fetch movies: ', error)
+            }
+        }
+
+        fetchMovies()
+    }, [])
 
     return (
         <StyledArticle>
