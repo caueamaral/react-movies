b930cee84c6feff56a66d446235094f0fd0681d7
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-13 16:36:31 -0300

    style: card hover overlay

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,8 +4,9 @@ import styled from 'styled-components'
 import type { MovieProps } from '../interfaces/MovieProps'
 
 const StyledSection = styled.section`
-    &:hover {
-        opacity: .9;
+    &:hover .card-overlay {
+        opacity: 1;
+        visibility: visible;
     }
 
     &:hover div svg {
@@ -24,18 +25,31 @@ const StyledImg = styled.img`
 
 const StyledFigcaption = styled.figcaption`
     padding-block: 7px;
+    position: relative;
+`
+
+const StyledOverlay = styled.div`
+    background: rgba(0, 0, 0, .5);
+    bottom: 0;
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: opacity 300ms;
+    visibility: hidden;
 `
 
 const StyledSvg = styled.svg`
     cursor: pointer;
     display: none;
-    fill: none;
-    height: 25px;
+    fill: #fff;
+    height: 20px;
     position: absolute;
     right: 15px;
     stroke: #fff;
     top: 15px;
-    width: 25px;
+    width: 20px;
 
     &.active{
         fill: #d00;
@@ -56,20 +70,21 @@ export default function Card({ movie }: { movie: MovieProps }) {
 
     return (
         <StyledSection className="card">
-            <StyledDiv>
-                <figure>
+            <figure>
+                <StyledDiv>
                     <StyledImg src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} alt={movie.title} />
-                    <StyledFigcaption>{movie.title}</StyledFigcaption>
-                </figure>
-                <StyledSvg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    className={`${favorite ? 'active' : ''}`}
-                    onClick={handleSvgClick}
-                >
-                    <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" />
-                </StyledSvg>
-            </StyledDiv>
+                    <StyledOverlay className="card-overlay" />
+                    <StyledSvg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        className={`${favorite ? 'active' : ''}`}
+                        onClick={handleSvgClick}
+                    >
+                        <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z" />
+                    </StyledSvg>
+                </StyledDiv>
+                <StyledFigcaption>{movie.title}</StyledFigcaption>
+            </figure>
         </StyledSection>
     )
 }
