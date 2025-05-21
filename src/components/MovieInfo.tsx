3d7b91783b23160c71b0b49c9a957580ed697cba
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-21 01:12:31 -0300

    style: MovieInfo structure

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,6 +1,32 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { styled } from 'styled-components'
 import getMovieById from '../services/getMovieById'
+
+const StyledSection = styled.section`
+    display: flex;
+    gap: 20px;
+`
+
+const StyledImg = styled.img`
+    width: 400px;
+`
+
+const StyledTitle = styled.h1`
+    font-size: 30px;
+    font-weight: 400;
+`
+
+const StyledReleaseDate = styled.div`
+    color: #aaa;
+    margin-top: 10px;
+`
+
+const StyledDescription = styled.p`
+    line-height: 1.5em;
+    margin-top: 30px;
+`
+
 export default function MovieInfo() {
     const { id, title } = useParams()
     const [movie, setMovie] = useState<any>(null)
@@ -23,11 +49,22 @@ export default function MovieInfo() {
         <>
             {movie && (
                 <article>
-                    <section>
-                        <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} alt={movie.title} />
-                        <h3>{movie.title}</h3>
-                        <p>{movie.overview}</p>
-                    </section>
+                    <StyledSection>
+                        <figure>
+                            <StyledImg src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} alt={movie.title} />
+                        </figure>
+                        <div>
+                            <StyledTitle>
+                                {movie.title}
+                            </StyledTitle>
+                            <StyledReleaseDate>
+                                {movie.release_date}
+                            </StyledReleaseDate>
+                            <StyledDescription>
+                                {movie.overview}
+                            </StyledDescription>
+                        </div>
+                    </StyledSection>
                 </article>
             )}
         </>
