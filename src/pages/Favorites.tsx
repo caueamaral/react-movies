d0dc162c05f63c7f074c268d9a84e06dfaab2711
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-23 19:34:36 -0300

    feat: show a message when there are no favorite movies

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,13 +1,37 @@
 import { useContext, useEffect } from 'react'
-import { SearchContext } from '../App'
+import { styled } from 'styled-components'
+import { FavoritesContext, SearchContext } from '../App'
 import FavoriteList from '../components/FavoriteList'
 
+const StyledMain = styled.main`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+`
+
+const StyledH1 = styled.h1`
+    font-size: 26px;
+
+    @media (min-width:1024px) {
+        font-size: 40px;
+    }
+`
+
 export default function Favorites() {
+    const { favorites } = useContext(FavoritesContext)
     const { setSearch } = useContext(SearchContext)
 
     useEffect(() => {
         setSearch('')
     }, [setSearch])
+
+    if (! favorites.length) return (
+        <StyledMain>
+            <section>
+                <StyledH1>You don't have any favorite movies yet</StyledH1>
+            </section>
+        </StyledMain>
+    )
 
     return (
         <main>
