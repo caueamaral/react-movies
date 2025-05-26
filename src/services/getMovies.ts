41927b5d355154aabd5e7ec9e6e93dfbc3ead73b
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-26 00:51:22 -0300

    refactor: use params object in getMovies

--- src/services/getMovies.ts
+++ src/services/getMovies.ts
@@ -2,13 +2,26 @@ import axios from 'axios'
 
 export default function getMovies(page: number) {
     const documentary = 99
-    const endpoint = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&without_genres=${documentary}&page=${page}&sort_by=popularity.desc`
 
     const headers = {
         'Authorization': `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
     }
 
-    const response = axios.get(endpoint, { headers })
+    const params = {
+        include_adult: false,
+        include_video: false,
+        language: 'en-US',
+        without_genres: documentary,
+        page,
+        sort_by: 'popularity.desc'
+    }
+
+    const endpoint = `https://api.themoviedb.org/3/discover/movie`
+
+    const response = axios.get(endpoint, {
+        headers,
+        params
+    })
 
     return response
 }
