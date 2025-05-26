6a08d153620b8bbf87487db194dae890c87cd68d
Cauê Amaral <caue.amaral@hotmail.com>
2025-05-26 00:22:44 -0300

    fix: prevent fetching documentaries using without_genres filter

--- src/services/getMovies.ts
+++ src/services/getMovies.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default function getMovies(page: number) {
-    const endpoint = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`
+    const documentary = 99
+    const endpoint = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&without_genres=${documentary}&page=${page}&sort_by=popularity.desc`
 
     const headers = {
         'Authorization': `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
