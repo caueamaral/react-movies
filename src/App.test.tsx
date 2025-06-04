185b5dc3af9c6142817035c0066fd808b98530e0
Cauê Amaral <caue.amaral@hotmail.com>
2025-06-04 10:28:01 -0300

    test: add component test for Navigation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'
 import App from './App';
+import Navigation from './components/Navigation'
 
-test('renders learn react link', () => {
-  render(<App />);
-  expect(screen.getByText(/movies/i)).toBeInTheDocument();
-});
+describe('renders the components', () => {
+  test('renders logo', () => {
+    render(<App />)
+    expect(screen.getByText(/movies/i)).toBeInTheDocument()
+  })
+
+  test('renders navigation', () => {
+    render(
+      <MemoryRouter>
+        <Navigation />
+      </MemoryRouter>
+    )
+    expect(screen.getByText(/home/i)).toBeInTheDocument()
+    expect(screen.getByText(/favorites/i)).toBeInTheDocument()
+  })
+})
